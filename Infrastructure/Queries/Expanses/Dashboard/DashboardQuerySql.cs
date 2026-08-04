ea16fac06ed3f9f75ee6660e5a13d7e009d96747
Vandecojjr <140183655+Vandecojjr@users.noreply.github.com>
2026-08-04 18:00:57 -0400

    fix: asjusta previsões do DashboardQuerySql.cs

--- Infrastructure/Queries/Expanses/Dashboard/DashboardQuerySql.cs
+++ Infrastructure/Queries/Expanses/Dashboard/DashboardQuerySql.cs
@@ -28,13 +28,29 @@ public static class DashboardQuerySql
                   (SELECT COALESCE(SUM(e."Amount"), 0)
                    FROM "Expenses" e
                             INNER JOIN "Members" m ON m."Id" = e."MemberId"
-                   WHERE  m."FamilyId" = @FamilyId) AS "TotalProjectedExpenditure",
+                   WHERE m."FamilyId" = @FamilyId
+                     AND (
+                           (e."Type" = 1 AND EXTRACT(MONTH FROM e."Date") = EXTRACT(MONTH FROM CURRENT_DATE)
+                                         AND EXTRACT(YEAR FROM e."Date") = EXTRACT(YEAR FROM CURRENT_DATE))
+                           OR
+                           (e."Type" = 2 AND e."Status" = true
+                                         AND e."StartDate" <= (DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month' - INTERVAL '1 day')
+                                         AND (e."EndDate" IS NULL OR e."EndDate" >= DATE_TRUNC('month', CURRENT_DATE)))
+                         )) AS "TotalProjectedExpenditure",
 
                   -- 4. Total Projected Income
                   (SELECT COALESCE(SUM(i."Amount"), 0)
                    FROM "Incomes" i
                             INNER JOIN "Members" m ON m."Id" = i."MemberId"
-                   WHERE m."FamilyId" = @FamilyId) AS "TotalProjectedIncome",
+                   WHERE m."FamilyId" = @FamilyId
+                     AND (
+                           (i."Type" = 1 AND EXTRACT(MONTH FROM i."Date") = EXTRACT(MONTH FROM CURRENT_DATE)
+                                         AND EXTRACT(YEAR FROM i."Date") = EXTRACT(YEAR FROM CURRENT_DATE))
+                           OR
+                           (i."Type" = 2 AND i."Status" = true
+                                         AND i."StartDate" <= (DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month' - INTERVAL '1 day')
+                                         AND (i."EndDate" IS NULL OR i."EndDate" >= DATE_TRUNC('month', CURRENT_DATE)))
+                         )) AS "TotalProjectedIncome",
 
                   -- 5. Total Balance
                   (SELECT COALESCE(SUM(w."CashBalance"), 0) + COALESCE(SUM(bc."DebitBalance"), 0)
@@ -66,6 +82,14 @@ public static class DashboardQuerySql
                   INNER JOIN "Categories" c ON c."Id" = i."CategoryId"
                   INNER JOIN "Members" m ON m."Id" = i."MemberId"
                   WHERE m."FamilyId" = @FamilyId
+                    AND (
+                          (i."Type" = 1 AND EXTRACT(MONTH FROM i."Date") = EXTRACT(MONTH FROM CURRENT_DATE)
+                                        AND EXTRACT(YEAR FROM i."Date") = EXTRACT(YEAR FROM CURRENT_DATE))
+                          OR
+                          (i."Type" = 2 AND i."Status" = true
+                                        AND i."StartDate" <= (DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month' - INTERVAL '1 day')
+                                        AND (i."EndDate" IS NULL OR i."EndDate" >= DATE_TRUNC('month', CURRENT_DATE)))
+                        )
                   GROUP BY c."Id", c."Name"
                   ;
 
@@ -75,6 +99,14 @@ public static class DashboardQuerySql
                   INNER JOIN "Categories" c ON c."Id" = e."CategoryId"
                   INNER JOIN "Members" m ON m."Id" = e."MemberId"
                   WHERE m."FamilyId" = @FamilyId
+                    AND (
+                          (e."Type" = 1 AND EXTRACT(MONTH FROM e."Date") = EXTRACT(MONTH FROM CURRENT_DATE)
+                                        AND EXTRACT(YEAR FROM e."Date") = EXTRACT(YEAR FROM CURRENT_DATE))
+                          OR
+                          (e."Type" = 2 AND e."Status" = true
+                                        AND e."StartDate" <= (DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month' - INTERVAL '1 day')
+                                        AND (e."EndDate" IS NULL OR e."EndDate" >= DATE_TRUNC('month', CURRENT_DATE)))
+                        )
                   GROUP BY c."Id", c."Name"
                   ;
                   """;
