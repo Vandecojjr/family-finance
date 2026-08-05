76447e24b120505553d42999ef2cfb8052b225f3
Vandecojjr <140183655+Vandecojjr@users.noreply.github.com>
2026-08-05 16:41:08 -0400

    fix: ajuste de sintaxe de query

--- Infrastructure/Queries/Expanses/Dashboard/DashboardQuerySql.cs
+++ Infrastructure/Queries/Expanses/Dashboard/DashboardQuerySql.cs
@@ -72,7 +72,7 @@ public static class DashboardQuerySql
                            LEFT JOIN "CreditCards" c ON c."BankAccountId" = b."Id"
                   WHERE w."FamilyId" = @FamilyId) AS "TotalCreditExpensed"
                   
-                  (COALESCE(SUM(b."CreditLimit"), 0) +  COALESCE(SUM(c."RemainingLimit"), 0)
+                  (SELECT COALESCE(SUM(b."CreditLimit"), 0) +  COALESCE(SUM(c."RemainingLimit"), 0)
                   FROM "BankAccounts" b
                            LEFT JOIN "Wallets" w ON b."WalletId" = w."Id"
                            LEFT JOIN "CreditCards" c ON c."BankAccountId" = b."Id"
